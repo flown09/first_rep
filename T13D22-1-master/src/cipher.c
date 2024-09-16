8e4091e93ada1976cfd68fae524a602c171e4335
flown09 <vlas442004@gmail.com>
2024-09-16 22:41:57 +0500

    Add files via upload

---
+++ T13D22-1-master/src/cipher.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int menu_1(char filename[256]);
+
+int main(void)
+{
+    int mode;
+    char garbage[256];
+    char filename[256];
+    while (1)
+    {
+        if (scanf("%d", &mode) != 1)
+        {
+            printf("n/a\n");
+            scanf("%255s", garbage);
+        }
+        if (mode == -1)
+            break;
+        else if (mode == 1)
+        {
+            menu_1(filename);
+        }
+    }
+
+    return 0;
+}
+
+int menu_1(char filename[256])
+{
+    fscanf(stdin, "%255s", filename);
+    FILE* fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+        printf("n/a\n");
+    }
+    else
+    {
+        fscanf(fp, "%255s", filename);
+        printf("%s", filename);
+        putchar('\n');
+    }
+}
